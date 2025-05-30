b3c342f086766e93c33cabaefb0d298e92aefaa8
Tian01301 <c.austria@toyota-mobilitysolutions.ph>
2025-05-30 14:28:57 +0800

    .

--- e2e/tests/test-self-drive-user.spec.ts
+++ e2e/tests/test-self-drive-user.spec.ts
@@ -403,6 +403,214 @@ test.describe('Self-Drive Full Process of Booking', () => {
     })
 })
 
+// test.describe('Verify user input data is retained through the booking flow', () => {
+//     test.slow()
+//     test('Self-Driven @smoke', async ({ book }) => {
+//         const start = 'September 23, 2025 | 08:30'
+//         const end = 'September 24, 2025 | 08:30'
+
+//         await book.homepage()
+//         await book.selfDriveProcess(
+//             'September', 23, '08:30',
+//             'September', 24, '08:30',
+//             'All Types'
+//         )
+
+//         // Verify the start and return date on Vehicle Selection
+//         await book.bookingDate(start, end)
+//         await book.waitForVehicleAvailabilityToLoad()
+//         await book.waitForImages()
+//         await book.choosePickVehicle()
+
+//         //Store all vehicle details to verify the accuracy on Summary Details.
+//         const elements = await book.page.locator("//div[@class='p-2']//div[@class='mb-3']").allTextContents();
+//         const [transmission, seats, fuel, yearModel, color, codingDay] = elements;
+
+//         await book.reserveCar()
+//         await expectLocatorTobeVisible(book.page.getByText("Booking Summary"))
+
+//         //Verify Booking Summary Details
+//         await book.bookingSummaryDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+
+//         await book.refundOptions('GCash')
+//         await book.enterAccountName('Spongbob Squarepants')
+//         await book.enterAccountNumber('09111174636')
+//         await book.clickCounter()
+//         await book.clickSubmit()
+//         await expectLocatorTobeVisible(book.page.getByText("Confirm Reservation"))
+
+//         //Verify Confirm Reservation Details
+//         await book.confirmReservationDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+
+//         await book.checkDocs()
+//         await book.clickProceed()
+
+//         const bookingID = await book.page.locator("//div[@id='copy-fill']").textContent();
+//         const plateNumber = await book.page.locator("section[class='mt-5 flex gap-x-5 gap-y-2 flex-wrap justify-center'] div div[class='text-xl font-semibold']").textContent()
+
+//         if (!bookingID) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         if (!plateNumber) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         await book.seeBookingList();
+
+//         //Verify the Details on Manage Booking Page
+//         await book.ViewDetails(bookingID);
+
+//         //Verify the Details on View Details Page of the Booking
+//         await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
+//         await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
+//     })
+
+
+//     test('Chauffeur-Driven @smoke', async ({ book }) => {
+//         const start = 'September 09, 2025 | 07:00'
+//         const end = 'September 09, 2025 | 16:00'
+
+//         await book.homepage()
+//         await book.chauffeurButton()
+//         await expectLocatorTobeVisible(book.chauffeur)
+//         await book.pickDate('September', 9)
+//         await book.pickTime('07:00')
+//         await book.returnDate('September', 9)
+//         await book.returnTime('16:00')
+
+//         await book.enterPickup('Kalayaan')
+//         await book.pickupLocation('Kalayaan Avenue, Makati City, Metro Manila')
+//         await book.enterDropoff('Sm Jazz')
+//         await book.dropoffLocation('SM Jazz Mall, Nicanor Garcia, Makati City, Metro Manila')
+
+//         await book.vehicleType('All Types')
+//         await book.page.waitForTimeout(3000)
+//         await book.checkAvailabilityNow()
+
+//         // Verify the start and return date on Vehicle Selection
+//         await book.bookingDate(start, end)
+//         await book.waitForVehicleAvailabilityToLoad()
+//         await book.waitForImages()
+//         await book.choosePickVehicle()
+
+//         //Store all vehicle details to verify the accuracy on Summary Details.
+//         const elements = await book.page.locator("//div[@class='p-2']//div[@class='mb-3']").allTextContents();
+//         const [transmission, seats, fuel, yearModel, color, codingDay] = elements;
+
+//         await book.reserveCar()
+//         await expectLocatorTobeVisible(book.page.getByText("Booking Summary"))
+
+//         //Verify Booking Summary Details
+//         await book.bookingSummaryDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+
+//         await book.refundOptions('GCash')
+//         await book.enterAccountName('Spongbob Squarepants')
+//         await book.enterAccountNumber('09111174636')
+//         await book.contactPerson()
+//         await book.clickCounter()
+//         await book.clickSubmit()
+//         await expectLocatorTobeVisible(book.page.getByText("Confirm Reservation"))
+
+//         //Verify Confirm Reservation Details
+//         await book.confirmReservationDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+
+//         await book.checkDocs()
+//         await book.clickProceed()
+
+//         const bookingID = await book.page.locator("//div[@id='copy-fill']").textContent();
+//         const plateNumber = await book.page.locator("section[class='mt-5 flex gap-x-5 gap-y-2 flex-wrap justify-center'] div div[class='text-xl font-semibold']").textContent()
+
+//         if (!bookingID) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         if (!plateNumber) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         await book.seeBookingList();
+
+//         //Verify the Details on Manage Booking Page
+//         await book.ViewDetails(bookingID);
+
+//         //Verify the Details on View Details Page of the Booking
+//         await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
+//         await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
+//     })
+
+//     test('Car Delivery @smoke', async ({ book }) => {
+//         const start = 'September 10, 2025 | 08:15'
+//         const end = 'September 10, 2025 | 11:00'
+
+//         await book.homepage()
+//         await book.carDelivery()
+//         await expectLocatorTobeVisible(book.carDeliveryButton)
+
+//         await book.pickDate('September', 10)
+//         await book.pickTime('08:15')
+//         await book.returnDate('September', 10)
+//         await book.returnTime('11:00')
+
+//         await book.carPickupDelivery('Kalayaan')
+//         await book.pickupLocation('Kalayaan Avenue, Makati City, Metro Manila')
+
+//         await book.vehicleType('All Types')
+//         await book.page.waitForTimeout(3000)
+//         await book.checkAvailabilityNow()
+
+//         // Verify the start and return date on Vehicle Selection
+//         await book.bookingDate(start, end)
+//         await book.waitForVehicleAvailabilityToLoad()
+//         await book.waitForImages()
+//         await book.choosePickVehicle()
+
+//         //Store all vehicle details to verify the accuracy on Summary Details.
+//         const elements = await book.page.locator("//div[@class='p-2']//div[@class='mb-3']").allTextContents();
+//         const [transmission, seats, fuel, yearModel, color, codingDay] = elements;
+
+//         await book.reserveCar()
+//         await expectLocatorTobeVisible(book.page.getByText("Booking Summary"))
+
+//         //Verify Booking Summary Details
+//         await book.bookingSummaryDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+//         await book.page.waitForTimeout(3000)
+
+//         await book.refundProcessCounter(
+//             'GCash',
+//             'Spongbob Squarepants',
+//             '09111174636'
+//         )
+
+//         await expectLocatorTobeVisible(book.page.getByText("Confirm Reservation"))
+
+//         //Verify Confirm Reservation Details
+//         await book.confirmReservationDetails(seats, fuel, transmission, codingDay, yearModel, color, start, end)
+
+//         await book.checkDocs();
+//         await book.clickProceed();
+
+//         const bookingID = await book.page.locator("//div[@id='copy-fill']").textContent();
+//         const plateNumber = await book.page.locator("section[class='mt-5 flex gap-x-5 gap-y-2 flex-wrap justify-center'] div div[class='text-xl font-semibold']").textContent()
+
+//         if (!bookingID) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         if (!plateNumber) {
+//             throw new Error("Booking ID not found!");
+//         }
+
+//         await book.seeBookingList();
+
+//         //Verify the Details on Manage Booking Page
+//         await book.ViewDetails(bookingID);
+
+//         //Verify the Details on View Details Page of the Booking
+//         await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
+//         await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
+//     })
+// })
 test.describe('Verify user input data is retained through the booking flow', () => {
     test.slow()
     test('Self-Driven @smoke', async ({ book }) => {
@@ -460,10 +668,6 @@ test.describe('Verify user input data is retained through the booking flow', () 
 
         //Verify the Details on Manage Booking Page
         await book.ViewDetails(bookingID);
-
-        //Verify the Details on View Details Page of the Booking
-        await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
-        await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
     })
 
 
@@ -533,10 +737,6 @@ test.describe('Verify user input data is retained through the booking flow', () 
 
         //Verify the Details on Manage Booking Page
         await book.ViewDetails(bookingID);
-
-        //Verify the Details on View Details Page of the Booking
-        await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
-        await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
     })
 
     test('Car Delivery @smoke', async ({ book }) => {
@@ -605,9 +805,5 @@ test.describe('Verify user input data is retained through the booking flow', () 
 
         //Verify the Details on Manage Booking Page
         await book.ViewDetails(bookingID);
-
-        //Verify the Details on View Details Page of the Booking
-        await expectLocatorTobeVisible(book.page.getByText('Payment Details'))
-        await book.bookingDetails(transmission, seats, fuel, codingDay, yearModel, plateNumber, start, end)
     })
 })
